52b7df82a2a64be07e8f7dd126124024add28a94
Aqdas <aqdasak@gmail.com>
2022-04-05 15:17:46 +0530

    bootstrap js for navbar collapse on phone added

--- admin_signup.php
+++ admin_signup.php
@@ -58,7 +58,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="#">Home</a>
+                        <a class="nav-link" aria-current="page" href="index.php">Home</a>
                     </li>
                     <?php require __DIR__ . '/partials/navbar/_login.php'; ?>
                     <?php require __DIR__ . '/partials/navbar/_dashboard.php'; ?>
@@ -136,6 +136,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         </div>
 
     </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 
 </html>
