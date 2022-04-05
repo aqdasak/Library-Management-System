a3edc944510fc1c39d2d315ab2419951d92bcbae
Aqdas <aqdasak@gmail.com>
2022-04-05 14:39:58 +0530

    bootstrap user signup form completed

--- admin_signup.php
+++ admin_signup.php
@@ -58,7 +58,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        <a class="nav-link" aria-current="page" href="#">Home</a>
                     </li>
                     <?php require __DIR__ . '/partials/navbar/_login.php'; ?>
                     <?php require __DIR__ . '/partials/navbar/_dashboard.php'; ?>
--- member_signup.php
+++ member_signup.php
@@ -45,33 +45,40 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="static/image/favicon.ico">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
     <title>Signup</title>
 </head>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" />
-<link rel="stylesheet" href="static/css/style.css">
 
 <body>
-    <header class="header">
-        <!-- Mid box for navbar -->
-        <div class="left">
-            <ul class="navbar">
-                <li><a href="index.php">Home</a></li>
-                <li><a href="member_login.php">Member Login</a></li>
-                <li><a href="admin_login.php">Admin Login</a></li>
-            </ul>
+    <!-- navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Library</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#">Home</a>
+                    </li>
+                    <?php require __DIR__ . '/partials/navbar/_login.php'; ?>
+                    <?php require __DIR__ . '/partials/navbar/_dashboard.php'; ?>
+                    <?php require __DIR__ . '/partials/navbar/_search_member.php'; ?>
+                    <?php require __DIR__ . '/partials/navbar/_logout.php'; ?>
+                </ul>
+                <form class="d-flex" action="search.php" method="GET">
+                    <input required name="query" class="form-control me-2" type="search" placeholder="Search by book or author" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
         </div>
-        <!-- Search -->
-        <div class="right">
-            <form action="search.php" class="d-flex">
-                <input name="query" method="POST" class="form-control me-2" type="search" class="round" placeholder="Search" aria-label="Search Book">
-                <button class="button btn-outline-success" type="submit">Search</button>
-            </form>
-        </div>
-    </header>
+    </nav>
+
 
     <div class="container">
-        <h1 class="main-title">Please Enter Your Details</h1>
 
         <center>
             <?php
@@ -88,28 +95,52 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             ?>
         </center>
 
-        <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
-            <div class="form-group">
-                <input required autofocus maxlength="10" type="text" name="Fname" placeholder="Firstname">
-            </div>
-            <div class="form-group">
-                <input required maxlength="30" type="text" name="Lname" placeholder="Lastname">
-            </div>
-            <div class="form-group">
-                <input required maxlength="30" type="email" name="email" placeholder="Email">
-            </div>
-            <div class="form-group">
-                <input required maxlength="255" type="password" name="password" placeholder="Password">
-            </div>
-            <div class="form-group">
-                <input required maxlength="255" type="password" name="cpassword" placeholder="Confirm password">
-            </div>
-            <div class="form-group">
-                <input oninput="javascript: if (this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);" maxlength="10" type="number" name="phone" placeholder="Phone no.">
-            </div>
+        <div class="main_div mt-4">
+            <h4>
+                <center><strong>Member Signup</strong></center>
+            </h4>
 
-            <center><button class="bottom-center">Submit</button></center>
-        </form>
+            <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
+
+                <div class="form-group row">
+                    <label for="fname" class="col-sm-2 col-form-label">Firstname</label>
+                    <div class="col-sm-10">
+                        <input id="fname" name="Fname" class="form-control mt-1" maxlength="10" type="text" placeholder="Firstname" required autofocus>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="lname" class="col-sm-2 col-form-label">Lastname</label>
+                    <div class="col-sm-10">
+                        <input id="lname" name="Lname" class="form-control mt-1" maxlength="30" type="text" placeholder="Lastname" required>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="email" class="col-sm-2 col-form-label">Email</label>
+                    <div class="col-sm-10">
+                        <input id="email" name="email" class="form-control mt-1" maxlength="30" type="email" placeholder="Email" required>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="phone" class="col-sm-2 col-form-label">Phone no.</label>
+                    <div class="col-sm-10">
+                        <input id="phone" class="form-control mt-1" oninput="javascript: if (this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);" maxlength="10" type="number" name="phone" placeholder="Phone no.">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="password" class="col-sm-2 col-form-label">Password</label>
+                    <div class="col-sm-10">
+                        <input id="password" name="password" class="form-control mt-1" maxlength="255" type="password" placeholder="Password" required>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="cpassword" class="col-sm-2 col-form-label">Confirm password</label>
+                    <div class="col-sm-10">
+                        <input id="cpassword" name="cpassword" class="form-control mt-1" maxlength="255" type="password" placeholder="Confirm password" required>
+                    </div>
+                </div>
+                <button type="submit" class="btn btn-primary mt-3">Submit</button>
+            </form>
+        </div>
     </div>
 </body>
 
